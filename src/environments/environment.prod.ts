264c286b1c3da04f5ce4fd2ce2fe854a7493e705
udani1997 <71671260+udani1997@users.noreply.github.com>
2025-06-25 21:38:09 +0530

    add new

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   defaultauth: "fackbackend",
   apiUrl: "https://synerisqcback-end-production.up.railway.app",
+  bucketUrl: "https://upload.gofile.io",
 };
